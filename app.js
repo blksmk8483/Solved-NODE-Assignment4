705a04d4ad60523609b6f79de87466a996dd3c05
Brandon Knight <knightbrandon1984@gmail.com>
2023-05-16 23:32:58 -0500

    intergrated into pug

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 const express = require('express');
 
 const app = express();
+const bodyParser = require('body-parser');
+
+const users = [];
+
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
+app.use(bodyParser({ extended: false }));
 
 app.get('/', (req, res, next) => {
-    res.render();
+    res.render('index', { pageTitle: 'Add User' });
 });
 
 app.get('/users', (req, res, next) => {
-    res.render();
+    res.render('users', { pageTitle: 'User' });
 });
 
 app.post('/add-user', (req, res, next) => {
+    users.push();
     res.redirect('/users');
 });
 
