9bb8dc44e8cae0dda90f72507e3a53a03afe7502
Brandon Knight <knightbrandon1984@gmail.com>
2023-05-23 11:25:00 -0500

    now in ejs

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const app = express();
 
 const users = [];
 
-app.engine('hbs', handlebars({defaultLayout: 'main-layout', extname: 'hbs'}));
-app.set('view engine', 'hbs');
+// app.engine('hbs', handlebars({defaultLayout: 'main-layout', extname: 'hbs'}));
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({ extended: false }));
