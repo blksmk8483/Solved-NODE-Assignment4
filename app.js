1ecac3425762bac29ee61e667473594d59ac3578
Brandon Knight <knightbrandon1984@gmail.com>
2023-05-23 10:15:23 -0500

    complete using pug

--- app.js
+++ app.js
@@ -8,18 +8,18 @@ const users = [];
 app.set('view engine', 'pug');
 app.set('views', 'views');
 
-app.use(bodyParser({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res, next) => {
     res.render('index', { pageTitle: 'Add User' });
 });
 
 app.get('/users', (req, res, next) => {
-    res.render('users', { pageTitle: 'User' });
+    res.render('users', { pageTitle: 'User', users: users });
 });
 
 app.post('/add-user', (req, res, next) => {
-    users.push();
+    users.push({ name: req.body.username });
     res.redirect('/users');
 });
 
